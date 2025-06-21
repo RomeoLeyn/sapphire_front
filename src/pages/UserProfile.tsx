f9b456a851321821802913e5e1451584fd8f1d03
RomeoLeyn <romanpituh228@gmail.com>
2025-06-21 22:08:00 +0300

    Updated loading component

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -42,7 +42,9 @@ const UserProfile = () => {
       {userInfo ? (
         <UserProfileInfo user={userInfo} onUpdate={handleUpdate} />
       ) : (
-        <p>Loading...</p>
+        <div className="flex items-center justify-center h-64">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+        </div>
       )}
     </div>
   );
